f8a0d7dcf10374427031e767e675a5c28c26eaa0
lyes <lyes.makhloufi@outlook.fr>
2020-06-30 17:18:19 +0200

    commit project

---
+++ src/main/java/com/lyes/WebMailApplication.java
@@ -0,0 +1,13 @@
+package com.lyes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMailApplication.class, args);
+	}
+
+}
